66664d9b2573843207abf0971109b30cfa82dc73
Umar Jalabu <uyakubu1@sheffield.ac.uk>
2023-06-13 16:18:37 +0100

    Working progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
 export default function App(){
   const [todos, setTodos] = useState([])
 
+  //function to set todo caption (title)
   function addTodo(title){
     setTodos(currentTodos => {
       return [...currentTodos, { id: crypto.randomUUID(),title, completed: false},
         ]
     })
   }
+
+  //function to select/deselect todo caption (title)
   function toggleTodo(id, completed){
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
@@ -22,6 +26,7 @@ export default function App(){
     })
   }
 
+  //function to delete todo caption (title)
   function deleteTodo(id){
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
@@ -31,21 +36,7 @@ export default function App(){
     <>{/**adding a fragment since we are returing 2 things (form) and (hi)...react only like 1 return item */}
     < NewTodoForm onSubmit = {addTodo}/>
     <h1 className="header">To-do List</h1>
-    <ul className="list">
-      {todos.length === 0 && "You do not have any to-dos'"} 
-      {todos.map(todo => {
-        return (          
-          <li key={todo.id}>
-          <label>
-            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-            {todo.title}
-          </label>
-          <button 
-          onClick={() => deleteTodo(todo.id)}
-          className="btn btn-danger">Delete</button>
-        </li> )
-      })}
-      </ul>
-    </>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </>
   )
 }
